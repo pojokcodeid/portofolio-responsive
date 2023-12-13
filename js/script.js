18c4b471f973af7deba98f93007b68c4ec4399b4
asep komarudin <pojokcodeid@gmail.com>
2023-12-13 07:14:05 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Get the button
+let mybutton = document.getElementById("scrollBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
